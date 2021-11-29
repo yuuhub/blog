4051f654dfeabead8d9a61b5d95e3e3b34e12ac3
yuuhub <71030036+yuuhub@users.noreply.github.com>
2021-11-29 12:57:08 +0800

    Generate rspec files

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: %i[ edit update destroy ]
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+    end
+
+    def new 
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        #@article.title = params[:title]
+        #@article.content = params[:content]
+
+        if @article.save
+            redirect_to articles_path
+        else
+            render :new
+        end
+    end
+    
+    def edit
+    end
+
+    def update
+        @article.update(article_params)
+        redirect_to articles_path
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pokemons
+  resources :articles
+  #get '/articles' => 'articles#index'
+  #get '/articles/new' => 'articles#new', as: 'new_article'
+  #post '/articles' => 'articles#create', as: 'create_article'
+
+  #get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+  #post '/articles' => 'articles#update', as: 'update_article'
+
+  #delete '/articles/:id' => 'articles#delete'
 end
